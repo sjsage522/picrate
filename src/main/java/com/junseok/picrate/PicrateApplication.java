9b25c26346fe043dfb8cf1c2f68e24b882b5fae6
Junseok Shin <sjsage522@gmail.com>
2023-02-02 17:51:41 +0900

    init

---
+++ src/main/java/com/junseok/picrate/PicrateApplication.java
@@ -0,0 +1,13 @@
+package com.junseok.picrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicrateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicrateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junseok/picrate/PicrateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junseok.picrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicrateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
