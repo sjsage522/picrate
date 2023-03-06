821d1df9a77ea5421d1eacd4cb0d216d746a7323
Junseok Shin <sjsage522@gmail.com>
2023-03-06 10:21:36 +0900

    modify: car api 수정 (평가 저장시 이미지 받는 방식 수정)

--- src/main/java/com/junseok/picrate/card/api/CardController.java
+++ src/main/java/com/junseok/picrate/card/api/CardController.java
@@ -8,8 +8,11 @@ import com.junseok.picrate.card.CardService;
 import com.junseok.picrate.card.dto.CardResponse;
 import com.junseok.picrate.rating.vo.RatingInfo;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +21,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.Base64;
 import java.util.List;
 
 @RequestMapping("api/v1")
@@ -34,14 +39,23 @@ public class CardController {
      * @param image  image
      * @param fields ratings
      * @return cardId
-     * @throws JsonProcessingException json parsing error
+     * @throws IOException
      */
     @PostMapping("/card")
-    public ResponseEntity<ApiResult<CardResponse>> uploadCard(@RequestParam("image") MultipartFile image, @RequestParam("fields") String fields) throws JsonProcessingException {
+    public ResponseEntity<ApiResult<CardResponse>> uploadCard(@RequestParam("image") String imageDataUrl, @RequestParam("fields") String fields) throws IOException {
         List<RatingInfo> ratings = new ObjectMapper().readValue(fields, new TypeReference<>() {});
 
+        // decode the image data URL into a byte array
+        byte[] imageBytes = Base64.getDecoder().decode(imageDataUrl.split(",")[1]);
+
+        // create a ByteArrayResource object from the byte array
+        ByteArrayResource imageResource = new ByteArrayResource(imageBytes);
+
+        // create a MultipartFile object from the ByteArrayResource object
+        MultipartFile image = new MockMultipartFile("image", imageResource.getFilename(), MediaType.APPLICATION_OCTET_STREAM_VALUE, imageResource.getInputStream());
+
         CardResponse cardResponse = cardService.uploadCard(image, ratings);
-        
+
         return new ResponseEntity<>(
             ApiResult.succeed(
                 CardResponse.builder()
