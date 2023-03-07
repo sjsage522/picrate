3de000725caa82bd368f1b068edbb8a625f11331
shinjunseok <sjsage522@gmail.com>
2023-03-07 16:22:42 +0900

    fix: 이미지 받는 방식 수정

--- src/main/java/com/junseok/picrate/card/api/CardController.java
+++ src/main/java/com/junseok/picrate/card/api/CardController.java
@@ -1,6 +1,5 @@
 package com.junseok.picrate.card.api;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.junseok.picrate.common.dto.ApiResult;
@@ -8,11 +7,8 @@ import com.junseok.picrate.card.CardService;
 import com.junseok.picrate.card.dto.CardResponse;
 import com.junseok.picrate.rating.vo.RatingInfo;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.Base64;
 import java.util.List;
 
 @RequestMapping("api/v1")
@@ -42,17 +37,8 @@ public class CardController {
      * @throws IOException
      */
     @PostMapping("/card")
-    public ResponseEntity<ApiResult<CardResponse>> uploadCard(@RequestParam("image") String imageDataUrl, @RequestParam("fields") String fields) throws IOException {
+    public ResponseEntity<ApiResult<CardResponse>> uploadCard(@RequestParam("image") MultipartFile image, @RequestParam("fields") String fields) throws IOException {
         List<RatingInfo> ratings = new ObjectMapper().readValue(fields, new TypeReference<>() {});
-
-        // decode the image data URL into a byte array
-        byte[] imageBytes = Base64.getDecoder().decode(imageDataUrl.split(",")[1]);
-
-        // create a ByteArrayResource object from the byte array
-        ByteArrayResource imageResource = new ByteArrayResource(imageBytes);
-
-        // create a MultipartFile object from the ByteArrayResource object
-        MultipartFile image = new MockMultipartFile("image", imageResource.getFilename(), MediaType.APPLICATION_OCTET_STREAM_VALUE, imageResource.getInputStream());
 
         CardResponse cardResponse = cardService.uploadCard(image, ratings);
 
